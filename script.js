ae43c49b2be2fa39e801b5d6a890e23d73d583f8
OrGanzian <organzian10@gmail.com>
2021-10-21 10:53:30 +0300

    acordion feature added with js functionality

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-const compareNow_btn = document.querySelector(".compare");
-compareNow_btn.addEventListener("click", () => alert("Got it!"));
-
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
+const acordionItem = document.querySelector(".acordions-container");
+acordionItem.addEventListener("click", function (e) {
+  if (e.target.parentElement.classList.contains("open-icon")) {
+    e.target.closest(".acordion-item").classList.toggle("open-acordion");
   }
-
-  printPerson() {
-    console.log(`my name is ${this.name}`);
-  }
-}
-
-class Student extends Person {
-  constructor(GPA, name, age) {
-    super(name, age);
-    this.GPA = GPA;
-  }
-}
-
-const or = new Student(85, `or`, 24);
-or.printPerson();
+});
